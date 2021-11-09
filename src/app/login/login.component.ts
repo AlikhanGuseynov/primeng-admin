9586c69720019e0d93854a015dd4a2bfec9fb834
Alixan <alikhan-gusejjnv@list.ru>
2021-11-09 22:28:40 +0400

    some

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../services/auth.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -11,8 +11,12 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {
+    this.route.params.subscribe(params => {
+      this.authService.setLoginStatus(false)
+    })
   }
 
   ngOnInit(): void {

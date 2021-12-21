bf905a48e63e6367ef73c1164c5b744ff7ca0468
Elvin Cafarov <elvinj@goldenpay.az>
2021-12-21 11:01:50 +0400

    login signup changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/some-admin'),
+      dir: require('path').join(__dirname, './coverage/tab'),
       subdir: '.',
       reporters: [
         { type: 'html' },
